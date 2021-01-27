2465458fc839c54384db243dfe61e9f9dac3b0bf
Snoy Kuo <snoykuo.twprotech@gmail.com>
2021-01-27 14:42:28 +0800

    result of all tries.

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.green),
       title: 'Hello Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text(
+            'Hello Flutter',
+            style: TextStyle(fontSize: 30, color: Colors.green),
+          ),
         ),
       ),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,8 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hello_flutter/main.dart';
 
 void main() {
-  testWidgets('hello test', (WidgetTester tester) async {
+  testWidgets('test_Hello_Flutter_shown', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
+    expect(find.text('Hello Flutter'), findsOneWidget);
   });
 }
